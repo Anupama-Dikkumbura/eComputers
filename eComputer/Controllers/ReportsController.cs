c81614d24ecd68564fca1b5a3fafbfc4c9b933c2
Anupama-Dikkumbura <anupamadikkubura@gmail.com>
2023-04-24 16:44:20 +0530

    Reports

--- eComputer/Controllers/ReportsController.cs
+++ eComputer/Controllers/ReportsController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eComputer.Data.Services;
+using eComputer.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,11 +13,41 @@ namespace eComputer.Controllers
 {
     public class ReportsController : Controller
     {
-        
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IOrdersService _ordersService;
+
+
+        public ReportsController(UserManager<ApplicationUser> userManager, IOrdersService ordersService)
         {
-            return View();
+            _userManager = userManager;
+            _ordersService = ordersService;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            var customers = await _userManager.GetUsersInRoleAsync("customer");
+            return View(customers);
+        }
+
+        public async Task<IActionResult> GenerateReport(string reportType)
+        {
+            if(reportType == "customers")
+            {
+                var customers = await _userManager.GetUsersInRoleAsync("customer");
+                return View("CustomerReport", customers);
+            }else if(reportType == "allOrders")
+            {
+                var allOrders = await _ordersService.GetAllAsync();
+                return View("AllOrders", allOrders);
+            }else if(reportType == "newOrders")
+            {
+                var newOrders = await _ordersService.GetAllAsync();
+                return View("NewOrders", newOrders);
+            }
+
+            return View("Index");
+        }
+
     }
 }
 
